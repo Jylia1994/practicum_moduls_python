441970d9b89328390a2186aeab681e0972e754f3
Jylia1994 <125107147+Jylia1994@users.noreply.github.com>
2023-04-24 11:42:41 +0800

    16.7.1

---
+++ cat.py
@@ -0,0 +1,11 @@
+class Cat:
+  def __init__(self, name, age, gender):
+    self.name = name
+    self.age = age
+    self.gender = gender
+  def getName(self):
+    return self.name
+  def getAge(self):
+    return self.age
+  def getGender(self):
+     return self.gender
