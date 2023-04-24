c785686d6c62efc4e6b2ba67664f8f43a74406bc
Jylia1994 <125107147+Jylia1994@users.noreply.github.com>
2023-04-24 11:42:59 +0800

    16.7.1

---
+++ Cat1.py
@@ -0,0 +1,14 @@
+from cat import Cat
+
+cat1 = Cat('Барон', '2 года', 'мальчик')
+
+print('Имя питомца', cat1.name)
+print('Возраст питомца', cat1.age)
+print('Пол питомца', cat1.gender)
+
+
+cat2 = Cat('Сем', '2 года', 'мальчик')
+
+print('Имя питомца', cat2.name)
+print('Возраст питомца', cat2.age)
+print('Пол питомца', cat2.gender)
